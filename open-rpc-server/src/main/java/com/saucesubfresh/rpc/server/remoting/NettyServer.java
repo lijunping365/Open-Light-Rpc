8df400a5e682cfbd9e56da8dc486ff583d028e7c
lijunping <2544054976@qq.com>
2022-06-08 08:10:18 +0800

    feat:netty 服务端

---
+++ open-rpc-server/src/main/java/com/saucesubfresh/rpc/server/remoting/NettyServer.java
@@ -0,0 +1,72 @@
+package com.saucesubfresh.rpc.server.remoting;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 该 server用于给各个 client 实例连接用。
+ * @author: 李俊平
+ * @Date: 2022-06-08 07:25
+ */
+@Slf4j
+public class NettyServer {
+
+    public void start(int port){
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(4);
+        try{
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                        .channel(NioServerSocketChannel.class)
+                        .handler(new LoggingHandler(LogLevel.INFO))
+                        .option(ChannelOption.SO_BACKLOG, 1024)
+                        // 保持长连接
+                        .childOption(ChannelOption.SO_KEEPALIVE, true)
+                        // 处理网络io事件，如记录日志、对消息编解码等
+                        .childHandler(new ChildChannelHandler());
+            //绑定端口，同步等待成功
+            ChannelFuture future = bootstrap.bind(port).sync();
+            Runtime.getRuntime().addShutdownHook(new Thread(()->{
+                bossGroup.shutdownGracefully(1000, 3000, TimeUnit.MILLISECONDS);
+                workerGroup.shutdownGracefully(1000, 3000, TimeUnit.MILLISECONDS);
+            }));
+            //等待服务器监听端口关闭
+            future.channel().closeFuture().sync();
+        } catch (Exception e){
+            log.error("netty server start failure");
+        } finally {
+            //优雅退出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+    /**
+     * handler类
+     */
+    private static class ChildChannelHandler extends ChannelInitializer<Channel> {
+
+        @Override
+        protected void initChannel(Channel ch) {
+            NettyServerHandler serverHandler = new NettyServerHandler();
+
+            ByteBuf delimiter = Unpooled.copiedBuffer(Constant.DELIMITER.getBytes());
+            ch.pipeline()
+                    .addLast(new DelimiterBasedFrameDecoder(Constant.MAX_LENGTH, delimiter))
+                    .addLast(new StringDecoder())
+                    .addLast(serverHandler);
+        }
+    }
+}
---
+++ open-rpc-server/src/main/java/com/saucesubfresh/rpc/server/remoting/NettyServerHandler.java
@@ -0,0 +1,34 @@
+package com.saucesubfresh.rpc.server.remoting;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 这里处理所有netty事件。
+ * @author: 李俊平
+ * @Date: 2022-06-08 08:04
+ */
+@Slf4j
+public class NettyServerHandler extends SimpleChannelInboundHandler<String> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String message) throws Exception {
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        log.error("some thing is error , " + cause.getMessage());
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        ctx.close();
+        super.channelInactive(ctx);
+    }
+}
