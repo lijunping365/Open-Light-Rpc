77dd3e1cb9f6791e55b4ca60c158152f304ef376
lijunping <2544054976@qq.com>
2022-08-11 21:02:06 +0800

    feat:优化

--- open-rpc-core/src/main/java/com/saucesubfresh/rpc/core/enums/Status.java
+++ open-rpc-core/src/main/java/com/saucesubfresh/rpc/core/enums/Status.java
@@ -4,6 +4,12 @@ package com.saucesubfresh.rpc.core.enums;
  * The server status
  */
 public enum Status {
+    /**
+     * server online
+     */
     ON_LINE,
+    /**
+     * server offline
+     */
     OFF_LINE
 }
