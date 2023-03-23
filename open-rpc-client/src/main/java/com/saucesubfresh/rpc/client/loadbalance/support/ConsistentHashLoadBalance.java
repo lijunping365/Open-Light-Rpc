a3e0977a6d5565b655f031d7196d2b545886c4e1
lijunping <2544054976@qq.com>
2023-03-23 17:07:19 +0800

    :memo: README

--- open-rpc-client/src/main/java/com/saucesubfresh/rpc/client/loadbalance/support/ConsistentHashLoadBalance.java
+++ open-rpc-client/src/main/java/com/saucesubfresh/rpc/client/loadbalance/support/ConsistentHashLoadBalance.java
@@ -28,9 +28,12 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 /**
- * 分组下机器地址相同，不同JOB均匀散列在不同机器上，保证分组下机器分配JOB平均；且每个JOB固定调度其中一台机器；
- * a、virtual node：解决不均衡问题
- * b、hash method replace hashCode：String 的 hashCode可能重复，需要进一步扩大 hashCode 的取值范围
+ * <pre>
+ * 一致性哈希算法
+ * 1. 使用虚拟节点解决不均匀问题
+ * 2. 使用扰动函数进一步扩大 hashCode 的取值范围
+ * 3. 不同任务均匀散列在不同机器上，保证每个任务固定调度其中一台机器
+ * </pre>
  *
  * @author lijunping on 2021/9/1
  */
